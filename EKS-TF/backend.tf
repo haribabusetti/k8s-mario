51569efe02783d8b870184cbaa72b827eef45c6e
haribabusetti <haribabusetti2002@gmail.com>
2024-10-22 13:23:32 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cherrybucket" # Replace with your actual S3 bucket name
+    bucket = "marcusbuckett" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
